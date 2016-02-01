ae527be2cfb3fe11848b00b1bdf50a154292349d
Shai Aharoni <saharoni@gmail.com>
2016-02-01 17:52:55 +0200

    first commit

---
+++ ImageClassifier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ImageClassifier
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(":");
+            int photoDatePropertyId = 36867;
+
+            var directory = ConfigurationManager.AppSettings["ImagesFolder"];
+
+            string[] files = Directory.GetFiles(directory);
+
+            foreach (string file in files)
+            {
+               
+                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                using (Image myImage = Image.FromStream(fs, false, false))
+                {
+                    string dateTaken = "N\\A";
+
+                    if (myImage.PropertyIdList.Contains(photoDatePropertyId))
+                    {
+                        PropertyItem propItem = myImage.GetPropertyItem(photoDatePropertyId);
+                        dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    }
+
+                    Console.WriteLine($"{file} : {dateTaken}");
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ImageMetadataExtractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageMetadataExtractor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ImageMetadataExtractor")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74d2b76f-75d8-4ce1-baaa-cc6fc43334bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

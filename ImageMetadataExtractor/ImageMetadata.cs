e275dd31850d0518a5ac5dca1db109a29c0b152e
Shai Aharoni <saharoni@gmail.com>
2016-02-08 17:08:17 +0200

    Added ImageMetadata

---
+++ ImageMetadataExtractor/ImageMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMetadataExtractor
+{
+    public class ImageMetadata
+    {
+        public string ProcessingSoftware { get; set; }
+        public DateTime? TakenAt { get; set; }
+        public GeoCoordinate Location { get; set; }
+
+    }
+}

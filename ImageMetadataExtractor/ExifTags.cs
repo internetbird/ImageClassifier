671c4f9fc030395f74ecf7bc96201f06fd5be2eb
Shai Aharoni <saharoni@gmail.com>
2016-03-13 17:33:31 +0200

    Added bing image downloader

---
+++ ImageMetadataExtractor/ExifTags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMetadataExtractor
+{
+    public enum ExifTags
+    {
+        GPSLatitudeRef = 1,
+        GPSLatitude = 2,
+        GPSLongitudeRef = 3,
+        GPSLongitude = 4,
+        ProcessingSoftware = 11,
+        DateTimeOriginal = 36867
+    }
+}
